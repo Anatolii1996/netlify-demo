fd70a984b7ae3da776fdd7ea2a25675b127f5b89
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-08-17 17:09:45 +0300

    add more routes

--- functions/api.js
+++ functions/api.js
@@ -8,5 +8,27 @@ router.get("/api/hello", function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+const isInvalidDate = (data) => data.toUTCString() === "Invalid Date";
+
+router.get("/api/", (req, res) => {
+  res.json({ unix: new Date().getTime(), utc: new Date().toUTCString() });
+});
+
+router.get("/api/:date?", (req, res) => {
+
+  const dateParam = req.params.date;
+
+  if (isInvalidDate(new Date(dateParam))&&isInvalidDate(new Date(+dateParam))) {
+    res.json({ error: "Invalid Date" });
+  } else{
+    if(isInvalidDate(new Date(dateParam))){
+      res.json({ unix: new Date(+dateParam).getTime(), utc: new Date(+dateParam).toUTCString() });
+    }else{
+      res.json({ unix: new Date(dateParam).getTime(), utc: new Date(dateParam).toUTCString()});
+    }
+  }
+
+});
+
 app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
