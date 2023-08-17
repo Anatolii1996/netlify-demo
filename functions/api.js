c834336c69c9de1f3be3e2aac53500ad7e5592be
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-08-17 16:40:24 +0300

    remove unless routes

--- functions/api.js
+++ functions/api.js
@@ -10,20 +10,8 @@ router.get('/', (req, res) => {
   res.send('App is running..');
 });
 
-//Create new record
-router.post('/add', (req, res) => {
-  res.send('New record added.');
-});
 
-//delete existing record
-router.delete('/', (req, res) => {
-  res.send('Deleted existing record');
-});
 
-//updating existing record
-router.put('/', (req, res) => {
-  res.send('Updating existing record');
-});
 
 //showing demo records
 router.get('/demo', (req, res) => {
